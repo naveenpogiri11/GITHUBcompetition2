ef5f23eb261f00dbc68013513e953c1caf787153
Naveen <chinna143na@gmail.com>
2025-03-26 11:19:39 -0400

    first three classes

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Person.cs
+public class Person
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string ID { get; set; }
+
+    public Person(string name, string email, string id)
+    {
+        Name = name;
+        Email = email;
+        ID = id;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, ID: {ID}";
+    }
+}
